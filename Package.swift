33f6050489ec58d1a22fad52424487a1e1aa847e
camtest079 <129403269+camtest079@users.noreply.github.com>
2024-10-09 13:49:20 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,7 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
         
         // Dependency 2: Conflicting version of Alamofire (4.x)
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
-        
-        // Dependency 3: Non-existent version of Alamofire (10.x)
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "10.0.0"),
-        
-        // Dependency 4: Invalid repository URL
-        .package(url: "https://github.com/some/nonexistent-repo.git", from: "1.0.0")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0")
     ],
     targets: [
         .target(
